97216a27e9ec7a4b322aaa2bb0015fd4c1e36c83
cborjamoreno <cesarborjamoreno@gmail.com>
2023-09-30 17:09:57 +0200

    shadow ray

--- Nori2/src/directwhitted.cpp
+++ Nori2/src/directwhitted.cpp
@@ -41,10 +41,10 @@ public:
 			// For that, we create a ray object (shadow ray),
 			// and compute the intersection
 			
-			// Ray3f shadowRay(its.p,emitterRecord.wi);
-			// Intersection shadowIts;
-			// if (scene->rayIntersect(shadowRay, shadowIts))
-			// 	continue;
+			Ray3f shadowRay(its.p,emitterRecord.wi);
+			Intersection shadowIts;
+			if (scene->rayIntersect(shadowRay, shadowIts))
+				continue;
 
 			// Finally, we evaluate the BSDF. For that, we need to build
 			// a BSDFQueryRecord from the outgoing direction (the direction
